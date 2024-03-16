cfb887416943ffc86a66d7fa25eb00730744af26
vidush11 <147984123+vidush11@users.noreply.github.com>
2024-03-16 11:54:58 +0530

    Update objective3.py
    Slight changes to functions for optimisation

--- objective3.py
+++ objective3.py
@@ -1,53 +1,21 @@
 import numpy as np
-import json 
-import matplotlib.pyplot as plt
+from time import time
+from sklearn.datasets import fetch_california_housing
+california_housing = fetch_california_housing(as_frame=True)
 
-def dist_calc_2(points,slope,x0,y0):
-    l=1
-    m=slope
+import json
 
-    vector_array=np.array([[m],[-l]])
-    vector_norm_array=vector_array/((vector_array**2).sum())**0.5
-
-    point_given=(x0,y0)
-
-    distance=(points-point_given)@vector_norm_array
+lats=list(california_housing.frame['Latitude'])
+longs=list(california_housing.frame['Longitude'])
+points=np.array([*zip(lats,longs)])
 
 
-    distance=np.abs(distance)
-    return distance.sum()
-
-def find_optimal_slope(points, x0, y0):
-    slopes = np.linspace(-10, 10, 10)  
-    min_distance = float('inf')
-    optimal_slope = None
-
-    for slope in slopes:
-        distance = dist_calc_2(points, slope, x0, y0)
-        if distance < min_distance:
-            min_distance = distance
-            optimal_slope = slope
-
-    return optimal_slope
-
-
-def dist(point,centroids):
-    x,y = point
-    min = 0
-    dist = 10**12
-    for i in range(len(centroids)):
-        temp = ((x-centroids[i][0])**2+(y-centroids[i][1])**2)**0.5
-        if temp < dist:
-            min = i
-            dist = temp
-    return min
-
-def centroid(points):
-    temp = points.sum(0)
-    x,y = temp[0]/points.shape[0],temp[1]/points.shape[0]
-    return x,y
-
+# xs=[x for x,y in points]
+# ys=[y for x,y in points]
+# print(min(xs),max(xs))
+# print(min(ys),max(ys))
 def orthogonal_regression(points):
+    points=np.array(points,dtype=complex)
     centroid_points=points.sum(0)/points.shape[0]
     cx,cy=centroid_points
     new_points=(points-centroid_points)*np.array([[1,1j]])
@@ -57,57 +25,156 @@ def orthogonal_regression(points):
     x,y=np.real(no),np.imag(no)
 
     slope=y/x
-    intercept=slope*(-cx)+cy
+    intercept=slope*-cx+cy
 
-    return slope,intercept
-
-with open(r'C:\Users\shank\Downloads\lats.txt') as f:
-    point = json.loads(f.read())
-
-minx = min([x[0] for x in point])
-maxx = max([x[0] for x in point])
-miny = min([x[1] for x in point])
-maxy = max([x[1] for x in point])
-x0, y0 = 0, 0
-for a,b in point:
-    x0+=a
-    y0+=b
-x0/=len(point)
-y0/=len(point)
-slope = find_optimal_slope(np.array(point),x0,y0)
-graph_trend = slope/abs(slope)
-cluster = 7
-range_ = np.linspace(minx,maxx,cluster)
-cluster_count = []
-p = maxy if graph_trend < 0 else miny
-q = maxy if p == miny else miny
-for i in zip(np.linspace(minx,maxx,cluster),np.linspace(p,q,cluster)):
-    cluster_count.append(list(i))
-line_set = []
-print(cluster_count)
-for a in range(20):
-    clusters = [[] for _ in range(len(cluster_count))]
-    x = 0; y = 0
-    for i in point:
-        temp = dist(i,cluster_count)
-        clusters[temp].append(i)
-    for i in range(len(clusters)): 
-        if clusters[i] != []:     
-            temp = np.array(clusters[i])
-            x,y = centroid(temp)
-            cluster_count[i] = [x,y]
+    return slope,np.real(intercept)
 
 
+def dist_calc(points,slope,intercept):
+    l=1
+    m=slope
+
+    vector_array=np.array([[m],[-l]])
+
+    vector_norm_array=vector_array/((vector_array**2).sum())**0.5
+
+    point_given=(0,intercept)
+
+
+
+    distance=(points-point_given)@vector_norm_array
+
+
+    distance=np.abs(distance)
+
+
+    return (distance.max(),distance.sum())
+
+def dist_calc_2(points,slope,intercept):
+    l=1
+    m=slope
+
+    vector_array=np.array([[m],[-l]])
+    vector_norm_array=vector_array/((vector_array**2).sum())**0.5
+
+    point_given=intercept
+
+    distance=(points-point_given)@vector_norm_array
+
+
+    distance=np.abs(distance).sum()
+    return (distance)
+# [(0,0),(1,1),(3,2)]
+
+def gradient_descent(points, learning_rate, slope, intercept):
+    ''' '''
+    l=1
+    m=slope
+
+    # vector_norm=(l/(l**2+m**2)**0.5,m/(l**2+m**2)**0.5)
+
+    vector_array=np.array([[m],[-l]])
+    vector_norm_array=vector_array/((vector_array**2).sum())**0.5
+
+    point_given=[0,intercept]
+
+    # print(points[:,0:1])
+    gradient_slope=( ((points-point_given)@vector_norm_array)*points[:,0:1] ).sum()
+
+    slope_step=gradient_slope*learning_rate
+    gradient_intercept=( ((points-point_given)@vector_norm_array) ).sum()
+    intercept_step=gradient_intercept*learning_rate
+
+    return slope_step,intercept_step
+
+iterations=1000
+init_slope=-1.11111111
+init_intercept=-77.83
+
+not_defined=set([np.inf,np.nan,-np.inf,-np.nan])
+
+print(dist_calc(points,init_slope,init_intercept))
+
+for i in range(iterations):
+    sl,inter=gradient_descent(points,10**-12,init_slope,init_intercept)
+
+    # print(dist_calc(points,init_slope,init_intercept))
+    init_slope-=sl
+    init_intercept-=inter
+
+# print(init_slope,init_intercept,s2-s1)
+
+print(dist_calc(points,init_slope,init_intercept))
+
+oslope,ointer=orthogonal_regression(points)
+print(oslope,ointer)
+print(dist_calc(points,oslope,ointer))
+print(dist_calc_2(points,-0.9090909090909101,(36.64196244420971, -120.6808155684726)))
+
+def optimal_shift(points,slope,intercept):
+    range=10
+    no=100
+    shift_vals=np.hstack([np.linspace(-range,range,no),0])
+
+    k=(slope**2+1)**0.5
+    shift_vals*=k
+    distances=[(optimal_slope(points,slope,shift+intercept),shift) for shift in shift_vals]
     
+    return min(distances)
+def optimal_slope(points,slope,intercept):
+    range=np.pi
+    no=10000
+    vals=np.linspace(-range,range,no)
 
-# print(cluster_count)
+    slopes=np.hstack([np.tan(vals),slope])
+    distances=[(dist_calc(points,new_slope,intercept)[1],new_slope) for new_slope in slopes]
+    
+    return min(distances)
+
+# print(optimal_shift(points,oslope,ointer))
+
+def dist(point,centroids):
+    distances=((centroids-point)**2).sum(1)
+    return np.argmin(distances)
+
+cc = 10
+xs=[x for x,y in points]
+ys=[y for x,y in points]
+
+
+graph_trend=-1
+
+cluster_centroids= np.array([list(i) for i in zip(np.linspace(min(xs),max(xs),cc),np.linspace(max(ys),min(ys),cc))])
+
+lines_set = []
+
+iterations=10
+
+for a in range(iterations):
+    clusters = [[] for _ in range(cc)]
+    x = 0; y = 0
+    for i in points:
+        temp = dist(i,cluster_centroids)
+        clusters[temp].append(i)
+
+    for ind, i in enumerate(clusters): 
+        if i:
+            xs,ys=zip(*i)
+            l=len(i)
+            cluster_centroids[ind]=[sum(xs)/l,sum(ys)/l]
+
 
 for i in clusters:
     temp = np.array(i)
-    slope, intercept = orthogonal_regression(temp)
-    line_set.append((slope,intercept))
+    if temp.size !=0:
+        slope, intercept = orthogonal_regression(temp)
+        lines_set.append((slope,intercept))
+    else:
+        lines_set.append((None,None))
+        #sorry we didn't implement when some of our k clusters are empty, we didn't have the time
 
    
 print("slopes and intercepts of the required k lines:")    
-print(line_set)
+print(lines_set)
+
 
